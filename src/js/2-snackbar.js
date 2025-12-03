23dd683e6b2cbbc0b5ba072c1658ed5d9b53e315
oleksii ponomarenko <alexponomarenko996@gmail.com>
2025-12-03 23:20:40 +0100

    update code

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,39 +1,42 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form');
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.form');
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
+  form.addEventListener('submit', event => {
+    event.preventDefault();
 
-  const delay = Number(form.delay.value);
-  const state = form.state.value;
+    //даю змінні для промісу
+    const delay = Number(form.delay.value); //значення, яке вкажем
+    const state = form.state.value; //fulfilled or rejected
 
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (state === 'fulfilled') {
-        resolve(delay);
-      } else {
-        reject(delay);
-      }
-    }, delay);
-  });
-
-  promise
-    .then(delay => {
-      iziToast.success({
-        title: 'Success',
-        message: `Fulfilled promise in ${delay}ms`,
-        position: 'topRight',
-      });
-    })
-    .catch(delay => {
-      iziToast.error({
-        title: 'Error',
-        message: `Rejected promise in ${delay}ms`,
-        position: 'topRight',
-      });
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
     });
 
-  form.reset();
+    promise
+      .then(delay => {
+        iziToast.success({
+          title: 'Success',
+          message: `Fulfilled promise in ${delay}ms`,
+          position: 'topRight', //позиція відображення повідомлення
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          title: 'Error',
+          message: `Rejected promise in ${delay}ms`,
+          position: 'topRight', //позиція відображення повідомлення
+        });
+      });
+
+    form.reset();
+  });
 });
